70d0a6c7c1237c90935abf6f3f9c5892996d5ece
beardyface <henryamwilliams@gmail.com>
2023-01-25 12:23:29 +1300

    Added upload_file to SlackBot

--- cares_lib/slack_bot/SlackBot.py
+++ cares_lib/slack_bot/SlackBot.py
@@ -7,3 +7,5 @@ class SlackBot:
   def post_message(self, channel, message):
     self.client.chat_postMessage(channel=channel,text=message)
   
+  def upload_file(self, channel, message, filepath, filename):
+    upload_text_file = self.client.files_upload(channels=channel, title=filename, file=filepath+filename, initial_comment=message)
