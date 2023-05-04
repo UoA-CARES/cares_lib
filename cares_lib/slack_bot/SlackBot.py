cf1570d52a71736d7975be5cbc3fc6f2a243730a
rainingx683 <yxin683@aucklanduni.ac.nz>
2023-05-04 13:03:08 +1200

    fix: error log

--- cares_lib/slack_bot/SlackBot.py
+++ cares_lib/slack_bot/SlackBot.py
@@ -20,11 +20,11 @@ class SlackBot:
   def __init__(self, slack_token):
     self.client = slack.WebClient(token=slack_token)
     
-  @exception_handler("Failed to reboot servos")
+  @exception_handler("Failed to post message")
   def post_message(self, channel, message):
     self.client.chat_postMessage(channel=channel,text=message)
 
-  @exception_handler("Failed to reboot servos")
+  @exception_handler("Failed to get message")
   def get_message(self, channel):
     conversation_id = self.get_id(channel)
     result = self.client.conversations_history(
@@ -36,7 +36,7 @@ class SlackBot:
     message = result["messages"][0]
     return message["text"]
 
-  @exception_handler("Failed to reboot servos")
+  @exception_handler("Failed to get ids")
   def get_id(self, channel_name):
     conversation_id = None
     for result in self.client.conversations_list():
@@ -47,5 +47,6 @@ class SlackBot:
           conversation_id = channel["id"]
           return conversation_id
   
+  @exception_handler("Failed to upload file")
   def upload_file(self, channel, message, filepath, filename):
-    upload_text_file = self.client.files_upload(channels=channel, title=filename, file=filepath+filename, initial_comment=message)
+    self.client.files_upload(channels=channel, title=filename, file=filepath+filename, initial_comment=message)
