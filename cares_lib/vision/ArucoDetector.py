acbf773c78c8330668f6f002f6a72f41f96aaff5
Manfred.Stoiber <Manfred.Stoiber@gmail.com>
2024-01-09 19:49:13 +1300

    refactor: use property "marker_size" of MarkerDetector base class and removed unused properties "dictionary" and "aruco_params" of MarkerDetector base class

--- cares_lib/vision/ArucoDetector.py
+++ cares_lib/vision/ArucoDetector.py
@@ -3,9 +3,9 @@ from cares_lib.vision.MarkerDetector import MarkerDetector
 
 class ArucoDetector(MarkerDetector):
     def __init__(self, marker_size, dictionary_id=cv2.aruco.DICT_4X4_50):
+        super().__init__(marker_size)
         self.dictionary = cv2.aruco.Dictionary_get(dictionary_id)
         self.aruco_params = cv2.aruco.DetectorParameters_create()
-        self.marker_size = marker_size
 
     def get_marker_poses(self, image, camera_matrix, camera_distortion, display=True):
         (corners, ids, rejected_points) = cv2.aruco.detectMarkers(
